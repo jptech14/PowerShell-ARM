91a9f12de423c028e3fa0aeb9cdb14c1bd75052b
JP <jerryperez06@gmail.com>
2025-08-05 15:42:33 -0400

    Add files via upload
    Create a New Virtual Machine

---
+++ VirtualMachines-Create-a-new-VM.ps1
@@ -0,0 +1,3 @@
+# Create a New Virtual Machine
+# Edit the ResourceGroupName, Name, Location and ImageName
+New-AzVM -ResourceGroupName "myRG" -Name "myVM" -Location "EastUS" -ImageName "Win2022Datacenter"
