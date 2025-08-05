df43c5ca5251c8d8f0ca53be684764c59211d0f6
JP <jerryperez06@gmail.com>
2025-08-05 15:07:02 -0400

    Add files via upload
    Get-AzStorageAccountKey

---
+++ StorageAccounts-Get-Storage-Account-Keys.ps1
@@ -0,0 +1,7 @@
+# List Storage Accounts
+# List Storage Account keys
+# Edit the ResourceGroupName and Name
+
+Get-AzStorageAccount
+
+Get-AzStorageAccountKey -ResourceGroupName "myRG" -Name "mystorageacct"
